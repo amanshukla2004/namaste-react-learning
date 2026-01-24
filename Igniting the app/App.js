328ca220041bff280d4afb1a9933ffdb9fdf2db7
Aman Shukla <as.amanshukla18@gmail.com>
2026-01-24 10:23:14 +0530

    made a ew folder named Igniting the app and learning about npm

---
+++ Igniting the app/App.js
@@ -0,0 +1,34 @@
+const headingElement = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello, React!",
+);
+
+console.log(headingElement);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(headingElement);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am an h1 tag inside child div"),
+  ),
+);
+console.log(parent);
+root.render(parent);
+
+const parent2 = React.createElement(
+  "div",
+  { id: "parent2" },
+
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag inside child div"),
+    React.createElement("h1", {}, "I am the second h1 tag inside child div"),
+  ]),
+);
+console.log(parent2);
+root.render(parent2);
